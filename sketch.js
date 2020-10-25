2ddc54d9d62f4ac795a84fa0294069fd5d2b5fb3
Shivam2006 <66618125+Shivam2006@users.noreply.github.com>
2020-10-25 19:56:58 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,40 @@
+function setup() {
+  createCanvas(displayWidth-100,displayHeight);
+ form=new Form();
+// regform=new Regform();
+
+ 
+}
+
+function draw() {
+  background(255,255,255);  
+form.display();
+ //regform.display();
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
